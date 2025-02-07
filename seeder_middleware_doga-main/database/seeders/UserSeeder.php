729a14a3f4a58ddd3fc942f09d9b3f35c44bde27
nemeserik1 <nemes.erik@nejanet.hu>
2025-02-07 08:43:21 +0100

    asd
    asd

--- seeder_middleware_doga-main/database/seeders/UserSeeder.php
+++ seeder_middleware_doga-main/database/seeders/UserSeeder.php
@@ -2,17 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\Destination;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DestinationSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Destination::factory(100)->create(); 
+        User::factory(50)->create(); 
     }
 }
