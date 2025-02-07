658dcf0bba62e0f919923b0ef5b14d7c7fbbe92c
nemeserik1 <nemes.erik@nejanet.hu>
2025-02-07 09:24:29 +0100

    asd
    asd

--- seeder_middleware_doga-main/resources/views/destinations/index.blade.php
+++ seeder_middleware_doga-main/resources/views/destinations/index.blade.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Destinations</h1>
+    <ol>
+        @foreach ($destinations as $dest)
+            <li>{{$dest->id}}</li>
+            <ul>
+                <li>{{$dest->destination}}</li>
+                <li>{{$dest->price}}</li>
+                <li>{{$dest->departure}}</li>
+                @if (auth()->check() && auth()->user()->role == 'user')
+                    <form action="{{ route('destination.show', $dest->id) }}" method="GET">
+                        <input type="submit" value="Kirándulás">
+                    </form>
+                @endif
+            </ul>
+        @endforeach
+    </ol>
 </body>
 </html>
