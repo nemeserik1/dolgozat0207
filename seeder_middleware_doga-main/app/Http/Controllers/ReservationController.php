79672fc6ca852f7f2613cb3f414339c56d833cdc
nemeserik1 <nemes.erik@nejanet.hu>
2025-02-07 09:47:50 +0100

    asd
    asd

--- seeder_middleware_doga-main/app/Http/Controllers/ReservationController.php
+++ seeder_middleware_doga-main/app/Http/Controllers/ReservationController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Destination;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
+    use HasFactory;
     public function index()
     {
-        
+         $destinationUser = Destination::with(['user','destination'])->get();
     }
 }
