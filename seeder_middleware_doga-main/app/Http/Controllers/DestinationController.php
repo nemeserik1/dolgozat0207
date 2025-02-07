203de3be5877e16a588f18543c27c9bd3f4b4248
nemeserik1 <nemes.erik@nejanet.hu>
2025-02-07 09:07:03 +0100

    asd
    asd

--- seeder_middleware_doga-main/app/Http/Controllers/DestinationController.php
+++ seeder_middleware_doga-main/app/Http/Controllers/DestinationController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Destination;
 use Illuminate\Http\Request;
 
 class DestinationController extends Controller
 {
-    //
+    public function index()
+    {
+        $destinations = Destination::all();
+        return view('destinatins.index', compact('destinations'));
+    }
+
 }
